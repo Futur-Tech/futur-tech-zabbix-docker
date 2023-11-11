d5daf529754f7c818e87d3ebbb0e9f0c44ef7b4d
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2023-11-11 12:42:13 +0100

    adding script to check image update

---
+++ bin/check_docker_image_updates.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export LOG_FILE="/var/log/futur-tech-zabbix-docker_image_updates.log"
+source /usr/local/bin/futur-tech-zabbix-docker/ft_util_inc_var
+
+# Initialize a counter for images with updates
+update_count=0
+
+# List all running Docker containers and get their image names
+containers=$(docker ps --format "{{.Image}}")
+
+# Loop through each image and check for updates
+for image in $containers; do
+    $S_LOG -s debug -d "$S_NAME" "Checking updates for $image..."
+
+    # Pull the latest version of the image
+    docker pull $image --quiet
+
+    # Compare the image ID of the running container with the latest image ID
+    running_image_id=$(docker images --format "{{.ID}}" --filter=reference="$image")
+    latest_image_id=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep "$image" | head -n 1)
+
+    if [ "$running_image_id" != "$latest_image_id" ]; then
+        $S_LOG -s warn -d "$S_NAME" "A newer version of $image is available."
+        update_count=$((update_count + 1))
+    fi
+done
+
+# Display summary message
+if [ $update_count -eq 0 ]; then
+    $S_LOG -d "$S_NAME" "All images are up-to-date."
+else
+    $S_LOG -s warn -d "$S_NAME" "$update_count image(s) have newer versions available."
+fi
--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,38 @@
 #!/usr/bin/env bash
 
+source "$(dirname "$0")/ft-util/ft_util_inc_func"
 source "$(dirname "$0")/ft-util/ft_util_inc_var"
+source "$(dirname "$0")/ft-util/ft_util_sudoersd"
+source "$(dirname "$0")/ft-util/ft_util_usrmgmt"
 
-APP_NAME="futur-tech-zabbix-docker"
-REQUIRED_PKG_ARR=( "at" )
+app_name="futur-tech-zabbix-docker"
+required_pkg_arr=("at")
 
-SRC_DIR="/usr/local/src/${APP_NAME}"
-SUDOERS_ETC="/etc/sudoers.d/${APP_NAME}"
+bin_dir="/usr/local/bin/${app_name}"
+src_dir="/usr/local/src/${app_name}"
 
 # Checking which Zabbix Agent is detected and adjust include directory
-$(which zabbix_agent2 >/dev/null) && ZBX_CONF_AGENT_D="/etc/zabbix/zabbix_agent2.d"
-# $(which zabbix_agentd >/dev/null) && ZBX_CONF_AGENT_D="/etc/zabbix/zabbix_agentd.conf.d" This template is only for Zabbix Agent 2
-if [ ! -d "${ZBX_CONF_AGENT_D}" ] ; then $S_LOG -s crit -d $S_NAME "${ZBX_CONF_AGENT_D} Zabbix Include directory not found" ; exit 10 ; fi
+$(which zabbix_agent2 >/dev/null) && zbx_conf_agent_d="/etc/zabbix/zabbix_agent2.d"
+# $(which zabbix_agentd >/dev/null) && zbx_conf_agent_d="/etc/zabbix/zabbix_agentd.conf.d" This template is only for Zabbix Agent 2
+if [ ! -d "${zbx_conf_agent_d}" ]; then
+  $S_LOG -s crit -d $S_NAME "${zbx_conf_agent_d} Zabbix Include directory not found"
+  exit 10
+fi
 
 $S_LOG -d $S_NAME "Start $S_DIR_NAME/$S_NAME $*"
 
 echo "
-  INSTALL NEEDED PACKAGES
+  INSTALL NEEDED PACKAGES & FILES
 ------------------------------------------"
 
-$S_DIR_PATH/ft-util/ft_util_pkg -u -i ${REQUIRED_PKG_ARR[@]} || exit 1
+$S_DIR_PATH/ft-util/ft_util_pkg -u -i ${required_pkg_arr[@]} || exit 1
+
+mkdir_if_missing "${bin_dir}"
+$S_DIR/ft-util/ft_util_file-deploy "$S_DIR/bin/" "${bin_dir}"
+$S_DIR/ft-util/ft_util_file-deploy "$S_DIR/ft-util/ft_util_log" "${bin_dir}/ft_util_log"
+$S_DIR/ft-util/ft_util_file-deploy "$S_DIR/ft-util/ft_util_inc_var" "${bin_dir}/ft_util_inc_var"
+$S_DIR/ft-util/ft_util_file-deploy "$S_DIR/etc.zabbix/${app_name}.conf" "${zbx_conf_agent_d}/${app_name}.conf"
+enforce_security exec "$bin_dir" zabbix
 
 echo "
   ZABBIX USER
@@ -27,24 +40,20 @@ echo "
 
 # Add zabbix user to sudo group
 if getent group docker | grep -q "\bzabbix\b"; then
-    $S_LOG -d $S_NAME "zabbix user is part of group sudo"
+  $S_LOG -d $S_NAME "zabbix user is part of group sudo"
 else
-    usermod -aG docker zabbix
-    $S_LOG -s $? -d $S_NAME "zabbix user has been added to group sudo (returned EXIT_CODE=$?)"
+  usermod -aG docker zabbix
+  $S_LOG -s $? -d $S_NAME "zabbix user has been added to group sudo (returned EXIT_CODE=$?)"
 fi
 
 echo "
   SETUP SUDOERS FILE
 ------------------------------------------"
 
-$S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
-
-echo "Defaults:zabbix !requiretty" | sudo EDITOR='tee' visudo --file=$SUDOERS_ETC &>/dev/null
-echo "zabbix ALL=(ALL) NOPASSWD:${SRC_DIR}/deploy-update.sh" | sudo EDITOR='tee -a' visudo --file=$SUDOERS_ETC &>/dev/null
-
-cat $SUDOERS_ETC | $S_LOG -d "$S_NAME" -d "$SUDOERS_ETC" -i 
-
-$S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
+bak_if_exist "/etc/sudoers.d/${app_name}"
+sudoersd_reset_file $app_name zabbix
+sudoersd_addto_file $app_name zabbix "${src_dir}/deploy-update.sh"
+show_bak_diff_rm "/etc/sudoers.d/${app_name}"
 
 echo "
   RESTART ZABBIX LATER
@@ -52,7 +61,5 @@ echo "
 
 echo "systemctl restart zabbix-agent*" | at now + 1 min &>/dev/null ## restart zabbix agent with a delay
 $S_LOG -s $? -d "$S_NAME" "Scheduling Zabbix Agent Restart"
-
-$S_LOG -d "$S_NAME" "End $S_NAME"
 
 exit
